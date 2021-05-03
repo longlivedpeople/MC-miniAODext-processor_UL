edbc9e2b9755e8a53a21317fd474eb39bf44affc
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2021-05-03 15:54:25 +0200

    Add 2016 postVFP configs

---
+++ Run2016-postVFP/crab_DYJetsToLL_M-50.py
@@ -0,0 +1,43 @@
+
+import CRABClient
+from CRABClient.UserUtilities import config
+
+config = config()
+
+# All output/log files go in directory workArea/requestName/
+#config.General.workArea = 'crab_projects'
+
+config.General.requestName = 'crab_DYJetsToLL_M-50'
+
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.General.instance = 'prod'
+
+# Set pluginName = Analysis if you are reading a dataset, or to PrivateMC if not (so you are generating events)
+config.JobType.pluginName = 'Analysis'
+# CMSSW cfg file you wish to run
+config.JobType.psetName = 'MC-RunIISummer20UL16MiniAODextended_cfg.py'
+# Increase virtual memory limit (sum needed by all threads) from default of 2000 MB.
+config.JobType.maxMemoryMB = 2500
+# Number of threads to use.
+#config.JobType.numCores = 2
+# To allow use of SL7 CMSSW versions that were SL6 at time of original DATA/MC production.
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16RECO-106X_mcRun2_asymptotic_v13-v2/AODSIM'
+config.Data.inputDBS = 'global'
+
+
+# Units of "totalUnits" and "unitsPerJob" (e.g. files, events, lumi sections)
+config.Data.splitting = 'FileBased'
+# Total number of these units to be processed.
+#config.Data.totalUnits = NTOTAL
+# Requested number in each subjob.
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = ## write user storage e.g. '/store/user/fernance/'
+config.Data.publication = True
+
+config.Data.outputDatasetTag = 'extended_RunIISummer20UL16MiniAOD'
+
+config.Site.storageSite = 'T2_ES_IFCA'
+
